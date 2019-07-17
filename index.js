d9faa4c82ebd029a73bd7386230a71d48409cdc8
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-07-17 15:52:37 +0530

    nodemon update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 //////////////////////////////////////////
@@ -43,7 +44,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
 
-
+const slugs = dataObj.map(el => slugify(el.productName, {lower:true}));
+console.log(slugs);
+//console.log(slugify('Amarnath Rajasekaran',{lower :true}));
 
 const server = http.createServer((req,res)=>{
     //console.log(req.url);
